25e75aa40fc3ce1830581ac36b2c3dfaee10f282
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-24 21:03:12 +0530

    move web3 funcitons to seperate file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import Web3 from 'web3'
 
+import { loadWeb3, loadBlockChainData } from './web3Functions'
+
 import { Navbar, Loader } from './components'
 
 import Container from 'react-bootstrap/Container'
@@ -9,17 +11,6 @@ import './App.css'
 
 function App() {
   const [loading, setLoading] = useState(false)
-
-  const loadWeb3 = async () => {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    } else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    } else {
-      window.alert('Non-Ethereum browser. Please install MetaMask.')
-    }
-  }
 
   useEffect(() => {
     loadWeb3()
