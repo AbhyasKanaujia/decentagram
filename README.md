3f755055a79a789985ed56dd28330b44f4b1aab2
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-24 12:35:48 +0530

    basic project setup

---
+++ README.md
@@ -0,0 +1,26 @@
+# Decentagram
+
+## Instagram
+
+- Instagram has a single web sever that hosts all the files.
+- The people who control instagram have full control over the plaform.
+- They can censor content and make specific conetet surface to more users.
+
+## Decentagram
+
+- Decentagram works on the blockchain
+- The files are stored in IPFS
+- There is a clear and transparent algorithm used to show content to the user.
+
+## Requirement for this Project
+
+1. **Node.js.** Used to install packages and run the client side application.
+2. **Truffle Framework.** A framework for creating etherium smart contract. Write, test and deploy.
+3. **Ganache.** Personal blockchain for testing.
+4. **Metamask.** Since browser do not directly connect to the blockchain, we use this to extend the browser.
+
+## Initial Setup
+
+1. `npx create-react-app .`
+2. `truffle init`
+3. Configure _truffle-config.js_
---
+++ truffle-config.js
@@ -0,0 +1,71 @@
+/**
+ *
+ * Hands-off deployment with Infura
+ * --------------------------------
+ *
+ * Do you have a complex application that requires lots of transactions to deploy?
+ * Use this approach to make deployment a breeze 🏖️:
+ *
+ * Infura deployment needs a wallet provider (like @truffle/hdwallet-provider)
+ * to sign transactions before they're sent to a remote public node.
+ * Infura accounts are available for free at 🔍: https://infura.io/register
+ *
+ * You'll need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. You can store your secrets 🤐 in a .env file.
+ * In your project root, run `$ npm install dotenv`.
+ * Create .env (which should be .gitignored) and declare your MNEMONIC
+ * and Infura PROJECT_ID variables inside.
+ * For example, your .env file will have the following structure:
+ *
+ * MNEMONIC = <Your 12 phrase mnemonic>
+ * PROJECT_ID = <Your Infura project id>
+ *
+ */
+
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const privateKeys = process.env.PRIVATE_KEYS || ''
+
+module.exports = {
+  /**
+   * You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
+
+  networks: {
+    development: {
+      host: '127.0.0.1', // Localhost (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
+      network_id: '*', // Any network (default: none)
+    },
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(
+          privateKeys.split(','), // Array of account private keys
+          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}` // Url to an Ethereum Node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 3,
+    },
+    contracts_build_directory: './src/abis/',
+  },
+
+  // Set default mocha options here, use special reporters, etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: '0.8.17',
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+}
